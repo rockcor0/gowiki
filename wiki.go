e225b16421865c091ba15474445222cc27461f48
Ricardo Delgado Carreño <rdelgaca@everis.com>
2021-09-03 14:45:43 -0500

    Redirect pages

--- wiki.go
+++ wiki.go
@@ -1,13 +1,33 @@
 package main
 
 import (
+	"errors"
 	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"regexp"
 )
 
-var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+var templates = template.Must(template.ParseFiles("tmpl/edit.html", "tmpl/view.html", "tmpl/index.html"))
+var validPath = regexp.MustCompile("^/(edit|save|view|index)/([a-zA-Z0-9]+)$")
+
+func main() {
+	//p1 := &Page{Title: "Test page", Body: []byte("This is a sample Page.")}
+	//p1.save()
+	//p2, _ := loadPage("Test page")
+	//fmt.Println(string(p2.Body))
+
+	//http.HandleFunc("/view/", viewHandler)
+	//http.HandleFunc("/edit/", viewHandler)
+	//http.HandleFunc("/save/", viewHandler)
+
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
+	http.HandleFunc("/", makeHandler(frontPageHandler))
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
 
 type Page struct {
 	Title string
@@ -16,13 +36,13 @@ type Page struct {
 
 //Save a txt file with page data
 func (p *Page) save() error {
-	filename := p.Title + ".txt"
+	filename := "data/" + p.Title + ".txt"
 	return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
 //Load page from txt file
 func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
+	filename := "data/" + title + ".txt"
 	body, err := ioutil.ReadFile(filename)
 
 	if err != nil {
@@ -32,9 +52,9 @@ func loadPage(title string) (*Page, error) {
 }
 
 //Implements an html template
-func renderTemplate(w http.ResponseWriter, tmpl string, p *Page){
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	//t, err := template.ParseFiles(tmpl + ".html")
-	err := templates.ExecuteTemplate(w, tmpl + ".html", p)
+	err := templates.ExecuteTemplate(w, tmpl+".html", p)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -45,8 +65,17 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page){
 	}*/
 }
 
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/view/"):]
+func frontPageHandler(w http.ResponseWriter, r *http.Request, title string) {
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, title, http.StatusNotFound)
+		return
+	}
+	renderTemplate(w, "index", p)
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+	//title := r.URL.Path[len("/view/"):]
 	p, err := loadPage(title)
 
 	if err != nil {
@@ -56,8 +85,8 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "view", p)
 }
 
-func editHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/edit/"):]
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+	//title := r.URL.Path[len("/edit/"):]
 	p, err := loadPage(title)
 
 	if err != nil {
@@ -66,8 +95,8 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 	renderTemplate(w, "edit", p)
 }
 
-func saveHandler(w http.ResponseWriter, r *http.Request) {
-	title := r.URL.Path[len("/save/"):]
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+	//title := r.URL.Path[len("/save/"):]
 	body := r.FormValue("body")
 	p := &Page{Title: title, Body: []byte(body)}
 	err := p.save()
@@ -78,14 +107,22 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
-func main() {
-	//p1 := &Page{Title: "Test page", Body: []byte("This is a sample Page.")}
-	//p1.save()
-	//p2, _ := loadPage("Test page")
-	//fmt.Println(string(p2.Body))
+func makeHandler(myFunc func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m := validPath.FindStringSubmatch(r.URL.Path)
+		if m == nil {
+			http.NotFound(w, r)
+			return
+		}
+		myFunc(w, r, m[2])
+	}
+}
 
-	http.HandleFunc("/view/", viewHandler)
-	http.HandleFunc("/edit/", editHandler)
-	http.HandleFunc("/save/", saveHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+	m := validPath.FindStringSubmatch(r.URL.Path)
+	if m == nil {
+		http.NotFound(w, r)
+		return "", errors.New("Invalid Page Title")
+	}
+	return m[2], nil //This is the second subexpression.
 }
